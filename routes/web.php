7d3bd71aa3bf0fbe70b0ced4c4e7c8253a2f5df7
siii noppall <naufal3544@gmail.com>
2024-11-19 09:54:01 +0700

    terbaru lagi

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Kontrol;
 use App\Http\Controllers\seckon;
+use App\Http\Controllers\NewsController;
 
 Route::get('/', function () {
     return ('Hello World');
@@ -10,3 +11,7 @@ Route::get('/', function () {
 
 Route::get('/kontrol',[Kontrol::class,'index']);
 Route::get('/seckon',[seckon::class,'index']);
+
+
+//News
+Route::get ('news', [NewsController::class, "index"]);
